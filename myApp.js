09c740e708f5d1f1982c8ff08ab09d90f88471f3
PrincessUrsula <64223855+PrincessUrsula@users.noreply.github.com>
2020-05-05 14:29:02 -0400

    var express = require('helmet');

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@
 * 4. Applied InfoSec Challenges
 * =============================
 ***********************************************/
-
+var express = require('helmet');
 var express = require('express'); // Do Not Edit
 var app = express();              // Do Not Edit
 
